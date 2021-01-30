c5dad72fa4a8a7954abb7b02e2bd376486a2cf7d
Kirill <example@ya.ru>
2021-01-31 00:19:58 +0300

    Added sign

---
+++ Assets/Scripts/SignScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SignScript : MonoBehaviour
+{
+    public GameObject lockedSign;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            lockedSign.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            lockedSign.SetActive(false);
+        }
+    }
+}

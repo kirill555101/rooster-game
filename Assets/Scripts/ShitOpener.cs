22f626e003ed7f263ea379f6513933fb66314af5
Kirill <example@ya.ru>
2021-01-31 04:32:32 +0300

    Some changes

---
+++ Assets/Scripts/ShitOpener.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShitOpener : MonoBehaviour
+{
+    public Sprite openedLock, closedLock;
+    public GameObject openedSign, closedSign;
+    public bool isActive = false;
+
+    GameObject door;
+
+    private void Start()
+    {
+        door = GameObject.Find("Дверь");
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (isActive)
+        {
+            return;
+        }
+
+        print(other.tag);
+        if (other.tag == "Shit" || other.tag == "Player")
+        {
+            GetComponent<SpriteRenderer>().sprite = openedLock;
+            door.GetComponent<BoxCollider2D>().enabled = false;
+            closedSign.SetActive(false);
+            openedSign.SetActive(true);
+
+            if (other.tag == "Shit")
+            {
+                isActive = true;
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (isActive)
+        {
+            return;
+        }
+
+        if (other.tag == "Player")
+        {
+            GetComponent<SpriteRenderer>().sprite = closedLock;
+            door.GetComponent<BoxCollider2D>().enabled = true;
+            closedSign.SetActive(true);
+            openedSign.SetActive(false);
+        }
+    }
+}
--- Assets/Scripts/SignScript.cs
+++ Assets/Scripts/SignScript.cs
@@ -4,10 +4,23 @@ using UnityEngine;
 
 public class SignScript : MonoBehaviour
 {
-    public GameObject lockedSign;
+    public GameObject lockedSign, openedSign;
+
+    ShitOpener shitOpener;
+
+    private void Start()
+    {
+        shitOpener = GameObject.Find("Кнопка открытия решеткки не нажатая").GetComponent<ShitOpener>();
+    }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
+        if (shitOpener.isActive)
+        {
+            openedSign.SetActive(true);
+            return;
+        }
+
         if (other.tag == "Player")
         {
             lockedSign.SetActive(true);
@@ -16,6 +29,12 @@ public class SignScript : MonoBehaviour
 
     private void OnTriggerExit2D(Collider2D other)
     {
+        if (shitOpener.isActive)
+        {
+            openedSign.SetActive(false);
+            return;
+        }
+
         if (other.tag == "Player")
         {
             lockedSign.SetActive(false);
